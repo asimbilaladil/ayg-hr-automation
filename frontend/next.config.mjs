f628db90fcd5d0de9f8ed91d8205df01269c04bf
Asim Bilal <asimbilal@mail.com>
2026-04-09 00:08:36 +0200

    update config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,17 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverActions: {
-      allowedOrigins: ['localhost:3000'],
+      allowedOrigins: ['localhost:3000', 'https://hr.aygfoods.com'],
     },
   },
-}
 
-export default nextConfig
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+export default nextConfig;
